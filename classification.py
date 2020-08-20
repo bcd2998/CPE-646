675cc6e5d99cdc7c9c45de139dabceaacaa56e24
errski <69652270+errski@users.noreply.github.com>
2020-08-20 10:15:52 -0400

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,9 +1,9 @@
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.preprocessing import StandardScaler
+#from mpl_toolkits.mplot3d import Axes3D
+#from sklearn.preprocessing import StandardScaler
 #import skimage.color
 #import skimage.io
 #import skimage.viewer
-import matplotlib.pyplot as plt # plotting
+#import matplotlib.pyplot as plt # plotting
 import numpy as np # linear algebra
 import os # accessing directory structure
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
@@ -14,21 +14,26 @@ books.dataframeName = 'book_data.csv'
 nRow, nCol = books.shape 
 print(f'There are {nRow} rows and {nCol} columns')
 
-
-print(books.head(20))
+######## Extra
+#print(books.head(20))
 
 #getting training set examples labels
-y_train=books['genres'].values
-x_train=books['book_rating'].values
+#y_train=books['genres'].values
+#x_train=books['book_rating'].values
 
-print(y_train)
+#print(y_train)
 
 from sklearn.model_selection import train_test_split
 #train_data,test_data,train_labels,test_labels=train_test_split(x_train,y_train,shuffle=True,test_size=0.25,random_state=42,stratify=y_train)
 #classes=np.unique(train_labels)
+
+# Show Unique Genres
 genres = np.unique(books['genres'])
 print(genres)
+##########
 
+
+##Encode Data for NB
 from sklearn.preprocessing import LabelEncoder
 gle = LabelEncoder()
 genre_labels = gle.fit_transform(books['genres'])
