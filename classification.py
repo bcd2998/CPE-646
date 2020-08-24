197b37cb519d7212e232708226d021beeb969812
errski <69652270+errski@users.noreply.github.com>
2020-08-24 18:41:20 -0400

    Add files via upload

--- classification.py
+++ classification.py
@@ -44,6 +44,113 @@ for x in range(len(genre_labels)):
 model = GaussianNB()
 model.fit(fields, title_labels)
 
+# Functions for prediction program
+## Predict Books
+def predict_books(input_genre,rating_start,rating_end):
+    """
+        Predicts books based on input genre and rating
+        Outputs - Dictionary of lists which contain book title, author, and rating
+    """
+    # Init Function
+    runique = np.unique(ratings_labels)
+    predictions = {}
+    output_predictions = {}
+    count = 1
+    
+    # Predict for each unique rating
+    for x in runique:
+        if ratings_mappings[x] >= rating_start and ratings_mappings[x] < rating_end:
+            # print(f'encode{x} = {ratings_mappings[x]}')
+            f'encode{x} = {ratings_mappings[x]}'
+            prediction = model.predict([[input_genre, x]])
+            predictions[x] = prediction
+
+    #Print Predictions
+    for p in predictions.keys():
+
+        # Get book name from title mapping
+        bookname = title_mappings[int(predictions[p])]
+
+        # Get index in dataframe to find location of book
+        title_index = books[books['book_title'] == bookname].index
+
+        # Pull Author and rating data from location
+        author = books.loc[title_index.values,'book_authors'].values[0]
+        rating = books.loc[title_index.values,'book_rating'].values[0]
+        
+        # Create an array to contain strings relating to book 
+        book_prediction = [bookname, author, rating]
+        
+        # Add book to output predictions    
+        if not book_prediction in output_predictions.values():
+            output_predictions[count] = book_prediction
+            count = count + 1
+    
+    return output_predictions
+    
+## Create Printable Object
+def create_printable_object(book_prediction_list):
+    """
+        This creates an object to print a list based on the dictionary of book predictions
+        Output - list of strings to print
+    """
+    # Init
+    forprint = []
+    for p in book_prediction_list.keys():
+        # Create printable list of predictions
+        printable = f'{p}: {book_prediction_list[p][0]} by {book_prediction_list[p][1]} (GR Rating {book_prediction_list[p][2]})'
+        forprint.append(printable)
+            
+    # Return printable list
+    return forprint
+
+## Print Predictions as a book list
+def print_books(forprint):
+    print(f'For Genre {genre_mappings[input_genre]}')
+    count = 1
+    for p in forprint:
+        print(p)
+    
+## Choose Book
+#def choose_book(predictions, rating):
+def choose_book(input_genre,rating_start,rating_end):
+    """
+        Recursive function checks titles with user 
+        Will do additional prediction to get a title if initial
+        prediction does not produce a book user wants
+        Output - Number for title of book selection
+    """
+    #Print Predictions as a book list
+    final_prediction = []
+    predictions = predict_books(input_genre,rating_start,rating_end)
+    forprint = create_printable_object(predictions)
+    print_books(forprint)
+    print('\n')
+    
+    # Inquire user about printed titles
+    print("Do any of these titles interest you?!\n")
+    rating_next = float(rating_start - 0.3)
+    yes = input("Y/N: ")
+    yes.lower()
+    
+    # Check User Input
+    if yes == 'y':
+        title_num = input("Which title interests you, input number: ")
+        final_prediction = predictions[int(title_num)]
+    elif yes == 'n':
+        # Check rating is not too low
+        if float(predictions[1][2]) > (rating_next+0.5):
+            print("If you do not like this selection, try again with a different genre.\n")
+            return predictions[1]
+        final_prediction = choose_book(input_genre,rating_next,rating_start)
+        
+    else:
+        print("This is not a valid input, try again.")
+        final_prediction = choose_book(input_genre,rating_start,rating_end)
+    
+    # Return the chosen book
+    return final_prediction
+
 #Getting user input for the genre that they are looking for
 # print("Welcome to the Book Recommender System!")
 # fictionOrNon = input("Please enter Fiction if you are looking for Fiction genres or Nonfiction if you are looking for Nonfiction genres: ")
@@ -119,50 +226,14 @@ model.fit(fields, title_labels)
 #     print("You did not enter a valid input!")
 #For loop getting numeric value
 
-# Predict this item/ Multiple Predictions
-#### Intention for some UI to determine genre and 
-##### code to predict at multiple rating values
-input_genre = 212
-runique = np.unique(ratings_labels)
-predictions = {}
-forprint = []
+###### Predict this item/ Multiple Predictions######
+# Specify Genre - Intention to get genre from user input
+input_genre = 200
 
-# Predict for each unique rating
-for x in runique:
-    if ratings_mappings[x] >= 4.6:
-        # print(f'encode{x} = {ratings_mappings[x]}')
-        f'encode{x} = {ratings_mappings[x]}'
-        prediction = model.predict([[input_genre, x]])
-        predictions[x] = prediction
+# Inquire about which title reader would like to see
+book_select = choose_book(input_genre,4.6,5.1)
 
-#Print Predictions
-for p in predictions.keys():
-    # prints for debug
-    #print(f'genre {genre_mappings[input_genre]}, rating{p}')
-    #print(f'predicts: {predictions[p]}')
-    #print(f'prediction:{title_mappings[int(predictions[p])]}')
-
-    # Some code to get more data
-    # Get book name from title mapping
-    bookname = title_mappings[int(predictions[p])]
-
-    # Get index in dataframe to find location of book
-    title_index = books[books['book_title'] == bookname].index
-
-    # Print location/ Entire entry in Dataframe
-    print(f'location{books.loc[title_index.values]}')
-
-    # Pull Author and rating data from location
-    author = books.loc[title_index.values,'book_authors'].values[0]
-    rating = books.loc[title_index.values,'book_rating'].values[0]
-    
-    # Create list of predictions
-    printable = f'{bookname} by {author} (GR Rating {rating})'
-    if not printable in forprint:
-        forprint.append(printable)
-
-#Print Predictions as a book list
-print(f'For Genre {genre_mappings[input_genre]}')
-for p in forprint:
-    print(p)
-    
+# Get index in dataframe to find location of book
+title_index = books[books['book_title'] == book_select[0]].index
+description = books.loc[title_index.values,'book_desc'].values[0]
+print(f'\nDescription of {book_select[0]} by {book_select[1]}: \n\n{description}\n')
